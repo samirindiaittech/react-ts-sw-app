cab3a2e5b521248ea2972df506ef8c614f465890
Samir Sanghvi <samir@spiraltechnolabs.com>
2023-10-20 19:01:05 +0530

    Deploy!
    Signed-off-by: Samir Sanghvi <samir@spiraltechnolabs.com>

--- src/utils/commonManagerItems.ts
+++ src/utils/commonManagerItems.ts
@@ -1,11 +1,10 @@
-import { useTargetInterval } from "./useTargetInterval"
+// import { useTargetInterval } from "./useTargetInterval"
 
 export const serviceWorkerRegistrationAutoUpdateAction = () => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    useTargetInterval(async () => {
+    /* useTargetInterval(async () => {
         try {
             // Get the Registration for the Service Worker
-            console.log("test")
             const serviceWorkerRegistration = await navigator?.serviceWorker?.getRegistration()
             if (serviceWorkerRegistration) {
                 // Check if there is a New Version of the Service Worker Available
@@ -14,5 +13,20 @@ export const serviceWorkerRegistrationAutoUpdateAction = () => {
         } catch (error) {
             console.error('Error during service worker update:', error);
         }
-    }, "swrauApplicationStorageKeyName", 60, 1000)
+    }, "swrauApplicationStorageKeyName", 60, 1000) */
+
+    setInterval(async () => {
+        try {
+            // Get the registration for the service worker.
+            const serviceWorkerRegistration = await navigator.serviceWorker.getRegistration();
+
+            if (serviceWorkerRegistration) {
+                // Check if there is a new version of the service worker available.
+                console.log("Hello Mr. Y", serviceWorkerRegistration);
+                await serviceWorkerRegistration.update();
+            }
+        } catch (error) {
+            console.error('Error during service worker update:', error);
+        }
+    }, 1 * 60 * 1000)
 }
