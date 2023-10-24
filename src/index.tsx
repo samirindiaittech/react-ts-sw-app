51ba93fb73f7fedec126372318b451b0e39e6973
Samir Sanghvi <samir@spiraltechnolabs.com>
2023-10-24 10:47:24 +0530

    Deploy..!!
    Signed-off-by: Samir Sanghvi <samir@spiraltechnolabs.com>

--- src/index.tsx
+++ src/index.tsx
@@ -21,7 +21,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-/* serviceWorkerRegistration.register({
+serviceWorkerRegistration.register({
   onUpdate: (registration) => {
     const waitingServiceWorker = registration.waiting
     if (waitingServiceWorker) {
@@ -42,9 +42,7 @@ root.render(
       waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' })
     }
   }
-}) */
-
-serviceWorkerRegistration.unregister()
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
